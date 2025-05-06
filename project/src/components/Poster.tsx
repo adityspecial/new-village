06be0bfb8eb733247ca60c0eeba966a7bcd0441d
YashX777 <yashas060205@gmail.com>
2025-05-06 23:01:20 +0530

    Created Poster.tsx

---
+++ project/src/components/Poster.tsx
@@ -0,0 +1,16 @@
+// components/Poster.tsx
+import React from 'react';
+
+const Poster = () => {
+  return (
+    <div className="w-full  bg-[#f7f7f7] pb-16">
+      <img
+        src="/images/Navbar/poster.png"
+        alt="Village Awareness Poster"
+        className="w-full object-contain max-h-[300px]"
+      />
+    </div>
+  );
+};
+
+export default Poster;
