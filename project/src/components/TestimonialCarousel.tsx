4581a6e89e7bbb3727469e122a903cd7dd0eb31b
PrajwalPrasad25 <CS23BT074@iitdh.ac.in>
2025-05-07 12:12:51 +0530

    Updated Volunteers Page

--- project/src/components/TestimonialCarousel.tsx
+++ project/src/components/TestimonialCarousel.tsx
@@ -29,7 +29,7 @@ const TestimonialCarousel: React.FC<CarouselProps> = ({ testimonials }) => {
               className={`absolute inset-0 transition-opacity duration-500 ${isActive ? 'opacity-100 z-20' : 'opacity-0 z-10'}`}
             >
               <div className="bg-white rounded-2xl shadow-xl overflow-hidden">
-                <div className="h-64">
+                <div className="h-64 p-4">
                   <img src={t.image} alt={t.author} className="w-full h-full object-top object-contain" />
                 </div>
                 <div className="p-8">
@@ -51,5 +51,26 @@ const TestimonialCarousel: React.FC<CarouselProps> = ({ testimonials }) => {
     </div>
   );
 };
+
+export const testimonials = [
+  {
+    content: `As an Agri-Business Management student, I’ve been volunteering with the Karnataka Incubation Foundation for nearly two years. During this time, I’ve contributed to rural development and farmer welfare initiatives, gaining hands-on experience with grassroots challenges. This journey has deepened my passion for agriculture and strengthened my commitment to creating a meaningful impact in the sector.`,
+    author: 'Prasannakumar P. Shiddagoudar',
+    role: '3rd Year Agri-Business Management, University of Agricultural Sciences, Dharwad',
+    image: '/public/images/volunteers/Prasanna.jpg',
+  },
+  {
+    content: `I was fortunate to be part of the team conducting training sessions for farmers, aimed at introducing them to new, sustainable farming techniques. The training covered everything from pest management to crop rotation. Through these sessions, farmers learned how to enhance their crop yields and adopt more sustainable methods. Knowing that my efforts directly contributed to improving their productivity and long-term practices was incredibly rewarding.`,
+    author: 'Rohit Kotagi',
+    role: 'B.Sc Hons agriculture, University of Agricultural Sciences, Dharwad',
+    image: '/public/images/volunteers/Rohit.jpg',
+  },
+  {
+    content: `As a volunteer with KIF, my main responsibility was to connect local farmers directly with customers, eliminating the need for middlemen. This initiative was crucial in ensuring farmers received fair prices for their produce and could build lasting, trust-based relationships with buyers. The direct connections I helped establish not only increased farmers' income but also improved their overall market access. It felt truly fulfilling to contribute to a model that supports farmer self-reliance and economic empowerment.`,
+    author: 'Suraj Naste',
+    role: '3rd Year Agri-Business Management, University of Agricultural Sciences, Dharwad',
+    image: '/public/images/volunteers/Suraj.jpg',
+  },
+];
 
 export default TestimonialCarousel;
